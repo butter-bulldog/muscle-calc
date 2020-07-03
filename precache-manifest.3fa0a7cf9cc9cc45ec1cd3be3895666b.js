39afa23e96f09738dcad7b17a970ab046c67f7da
FumiakiSaito <fumiakisaito@FumiakiSaitonoMacBook-ea.local>
2020-07-04 01:51:18 +0900

    Updates

---
+++ precache-manifest.3fa0a7cf9cc9cc45ec1cd3be3895666b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738bf34f72b41ee52cdfd02e3812f69d",
+    "url": "/muscle-calc/index.html"
+  },
+  {
+    "revision": "9d0c10d1641930be94dd",
+    "url": "/muscle-calc/static/js/2.80a4d0bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/muscle-calc/static/js/2.80a4d0bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c2d97738659257e4ca",
+    "url": "/muscle-calc/static/js/main.90079f31.chunk.js"
+  },
+  {
+    "revision": "18253246f4b514e3c6e1",
+    "url": "/muscle-calc/static/js/runtime-main.40ad7d01.js"
+  }
+]);
